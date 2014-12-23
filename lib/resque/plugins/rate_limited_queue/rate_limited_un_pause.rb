e55f39dead23cd7d9f7a6163a714c8ea7db2dab9
pavoni <greg@xenapto.com>
2014-12-23 11:10:54 +0000

    Add missed files

---
+++ lib/resque/plugins/rate_limited_queue/rate_limited_un_pause.rb
@@ -0,0 +1,37 @@
+module Resque
+  module Plugins
+    module RateLimitedQueue
+      class UnPause
+        @queue = nil
+
+        class << self
+          attr_writer(:queue)
+
+          def use?
+            Resque.respond_to?(:enqueue_at_with_queue) && @queue
+          end
+
+          def enqueue(timestamp, klass)
+            # If Resque scheduler is installed and queue is set - use it to queue a wake up job
+            return unless use?
+            Resque.enqueue_at_with_queue(
+              @queue,
+              timestamp,
+              Resque::Plugins::RateLimitedQueue::UnPause,
+              klass)
+          end
+
+          def perform(klass)
+            class_from_string(klass.to_s).un_pause
+          end
+
+          def class_from_string(str)
+            str.split('::').reduce(Object) do |mod, class_name|
+              mod.const_get(class_name)
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/resque/plugins/rate_limited_queue/version.rb
@@ -0,0 +1,3 @@
+module ResqueRateLimitedQueue
+  VERSION = "0.0.9"
+end
