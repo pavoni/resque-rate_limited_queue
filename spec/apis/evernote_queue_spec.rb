77c0353ab04483f858c31c0718d6473225c7d127
Dominic Sayers <dominic@sayers.cc>
2016-01-15 13:22:52 +0000

    Fix non-rate-limit exception exception

--- spec/apis/evernote_queue_spec.rb
+++ spec/apis/evernote_queue_spec.rb
@@ -9,7 +9,17 @@ end
 
 class RateLimitedTestQueueEn
   def self.perform(succeed)
-    fail(Evernote::EDAM::Error::EDAMSystemException, rateLimitDuration: RateLimitDuration) unless succeed
+    fail(
+      Evernote::EDAM::Error::EDAMSystemException,
+      errorCode: Evernote::EDAM::Error::EDAMErrorCode::RATE_LIMIT_REACHED,
+      rateLimitDuration: RateLimitDuration
+    ) unless succeed
+  end
+end
+
+class RateLimitedTestQueueOther
+  def self.perform
+    fail(Evernote::EDAM::Error::EDAMSystemException)
   end
 end
 
@@ -49,6 +59,17 @@ describe Resque::Plugins::RateLimitedQueue::EvernoteQueue do
         Resque::Plugins::RateLimitedQueue::EvernoteQueue.should_receive(:pause_until)
         Resque::Plugins::RateLimitedQueue::EvernoteQueue
           .enqueue(RateLimitedTestQueueEn, false)
+      end
+    end
+
+    context 'with exception that is not rate limit' do
+      before do
+        Resque::Plugins::RateLimitedQueue::EvernoteQueue.stub(:rate_limited_requeue)
+      end
+      it 'raises the exception when request fails' do
+        expect do
+          Resque::Plugins::RateLimitedQueue::EvernoteQueue.enqueue(RateLimitedTestQueueOther)
+        end.to raise_error Evernote::EDAM::Error::EDAMSystemException
       end
     end
   end
